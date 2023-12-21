2dbb0b7e7567f7b9fe8725f73322c8df7d0b7f03
shree-77 <shreedhoni7@gmail.com>
2023-12-21 20:24:01 +0530

    qn 1st completed

---
+++ src/question1/FindSmallest.java
@@ -0,0 +1,27 @@
+package question1;
+
+/**
+ * Problem Statement: Given an array, we have to find the smallest element in the array
+ * Example 1:
+ * Input: arr[] = {2,5,1,3,0};
+ * Output: 0
+ * Explanation: 0 is the smallest element in the array.
+ */
+
+public class FindSmallest {
+    public static void main(String[] args) {
+        int[]arr={2,5,1,3,0};
+        System.out.println(findSmall(arr));
+    }
+    public static int findSmall(int [] arr){
+        int ans = Integer.MAX_VALUE;
+
+        for(int i:arr){
+            if(i<ans){
+                ans=i;
+            }
+        }
+        return ans;
+
+    }
+}
